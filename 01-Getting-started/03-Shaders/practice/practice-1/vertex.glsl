5cc1e6a86d73943cb5010f86a86324690d876291
simpidbit <2766277616@qq.com>
2021-02-08 09:08:30 +0800

    更新spshader.h

--- 01-Getting-started/03-Shaders/practice/practice-1/vertex.glsl
+++ 01-Getting-started/03-Shaders/practice/practice-1/vertex.glsl
@@ -6,6 +6,6 @@ out vec4 vertexColor;
 
 void main()
 {
-    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+    gl_Position = vec4(-aPos.x, -aPos.y, -aPos.z, 1.0);
     vertexColor = vec4(aColor.xyz, 1.0);
 }
--- 01-Getting-started/03-Shaders/shader-obj/spshader.h
+++ 01-Getting-started/03-Shaders/shader-obj/spshader.h
@@ -11,3 +11,17 @@ struct SPShader {
 
 void spinit_shader(struct SPShader *shader, const char *vfilename, const char *ffilename);
 void spuse_shader(struct SPShader *shader);
+
+void set_uniform_1i(struct SPShader *shader, const char *name, int value);
+void set_uniform_1f(struct SPShader *shader, const char *name, float value);
+void set_uniform_2i(struct SPShader *shader, const char *name,
+                    int value1, int value2);
+void set_uniform_2f(struct SPShader *shader, const char *name, float value1, float value2);
+void set_uniform_3i(struct SPShader *shader, const char *name,
+                    int value1, int value2, int value3);
+void set_uniform_3f(struct SPShader *shader, const char *name,
+                    float value1, float value2, float value3);
+void set_uniform_4i(struct SPShader *shader, const char *name,
+                    int value1, int value2, int value3, int value4);
+void set_uniform_4f(struct SPShader *shader, const char *name,
+                    float value1, float value2, float value3, float value4);
